47934becc47c30b3659170ad100230890c6a3210
Robert Ashton <r.ashton@imperial.ac.uk>
2022-08-22 16:24:40 +0100

    Split overrides arg to improve interface

--- R/options.R
+++ R/options.R
@@ -3,59 +3,60 @@
 #' This gets the JSON for the model or calibration options for a particular
 #' country. The control info will be filled in in this order of precedence
 #' 1. hardcoded default values from `default_options.csv`
-#' 2. `overrides` arg
-#' 3. Fallback value in `controls.R`
+#' 2. `fallback_values` arg (if set)
+#' 3. Fallback value in `controls.R` (if set)
 #'
 #' @param type The type of options to get, model or calibration
 #' @param iso3 The iso3 of the country to get options for
-#' @param overrides A list of options and/or values to override those in JSON
-#' template. Must be in the format of a list of controls i.e.
+#' @param options A named list of options to override those in JSON
+#' template. Must be in the format of a list of control options i.e.
 #' list(
 #'   control_id = list(
-#'     options = list(
-#'       list(
-#'         id = "1",
-#'         label = "one"
-#'       ),
-#'       list(
-#'         id = "2",
-#'         label = "two"
-#'       )
+#'     list(
+#'       id = "1",
+#'       label = "one"
 #'     ),
-#'     value = "1"
-#'   )
+#'     list(
+#'       id = "2",
+#'       label = "two"
+#'     )
+#'   ),
+#'   ...
 #' )
+#' @param fallback_values A list of names values to use as fallbacks if
+#' hardcoded defaults are invalid.
 #'
 #' @return The complete controls JSON
 #' @export
-get_controls_json <- function(type, iso3, overrides) {
+get_controls_json <- function(type, iso3, options, fallback_values) {
   ## TODO: Assert inputs are sensible
   switch(type,
-         "model" = build_model_controls(iso3, overrides),
-         "calibration" = build_calibration_controls(iso3, overrides),
+         "model" = build_model_controls(iso3, options, fallback_values),
+         "calibration" = build_calibration_controls(iso3, options,
+                                                    fallback_values),
          stop(t_("OPTIONS_UNKNOWN_TYPE", list(type = type))))
 }
 
-build_model_controls <- function(iso3, overrides) {
+build_model_controls <- function(iso3, options, fallback_values) {
   template <- read_template("model_options.json")
 
   controls <- get_model_controls()
-  controls <- set_options_overrides(controls, overrides)
+  controls <- set_options(controls, options)
 
   defaults <- read_hardcoded_defaults(iso3, controls)
-  controls <- set_values(controls, defaults, overrides)
+  controls <- set_values(controls, defaults, fallback_values)
 
   build_json(template, controls)
 }
 
-build_calibration_controls <- function(iso3, overrides) {
+build_calibration_controls <- function(iso3, options, fallback_values) {
   template <- read_template("calibration_options.json")
 
   controls <- get_calibration_controls()
-  controls <- set_options_overrides(controls, overrides)
+  controls <- set_options(controls, options)
 
   defaults <- read_hardcoded_defaults(iso3, controls)
-  controls <- set_values(controls, defaults, overrides)
+  controls <- set_values(controls, defaults, fallback_values)
 
   build_json(template, controls)
 }
@@ -72,11 +73,11 @@ has_options <- function(control) {
   !is.null(control) && control$type %in% c("select", "multiselect")
 }
 
-set_options_overrides <- function(controls, overrides) {
+set_options <- function(controls, options) {
   for (id in names(controls)) {
     if (has_options(controls[[id]])) {
-      if (!is.null(overrides[[id]]$options)) {
-        controls[[id]]$options <- overrides[[id]]$options
+      if (!is.null(options[[id]])) {
+        controls[[id]]$options <- options[[id]]
       }
       if (is.null(controls[[id]]$options) ||
           length(controls[[id]]$options) == 0) {
@@ -87,7 +88,7 @@ set_options_overrides <- function(controls, overrides) {
   controls
 }
 
-set_values <- function(controls, defaults, overrides) {
+set_values <- function(controls, defaults, fallback_values) {
   for (control_id in names(controls)) {
     get_value <- switch(controls[[control_id]]$type,
                         "select" = set_select_value,
@@ -96,50 +97,51 @@ set_values <- function(controls, defaults, overrides) {
                         stop("Unknown control type"))
     controls[[control_id]]$value <- get_value(controls[[control_id]],
                                               defaults[[control_id]],
-                                              overrides[[control_id]])
+                                              fallback_values[[control_id]])
   }
   recursive_scalar(controls)
 }
 
-set_select_value <- function(control, default, override) {
+set_select_value <- function(control, default, fallback) {
   possible_ids <- lapply(control$options, "[[", "id")
   ## Valid if not set (e.g. empty string) or it is a possible ID
   is_valid <- function(x) !is.null(x) && (x == "" || x %in% possible_ids)
   if (is_valid(default)) {
     value <- default
-  } else if (is_valid(override$value)) {
-    value <- override$value
+  } else if (is_valid(fallback)) {
+    value <- fallback
   } else {
     value <- NULL
   }
   value
 }
 
-set_multiselect_value <- function(control, default, override) {
+set_multiselect_value <- function(control, default, fallback) {
   possible_ids <- lapply(control$options, "[[", "id")
   ## Valid if empty string or each item is in list of possible IDs
   is_single_valid <- function(y) {
     length(y) == 1 && y %in% possible_ids
   }
   is_valid <- function(x) {
-    !is.null(x) && (x == "" || all(vapply(x, is_single_valid, logical(1))))
+    !is.null(x) && (
+      isTRUE(x == "") || all(vapply(x, is_single_valid, logical(1))))
   }
   if (is_valid(default)) {
     value <- default
-  } else if (is_valid(override$value)) {
-    value <- override$value
+  } else if (is_valid(fallback)) {
+    value <- fallback
   } else {
     value <- NULL
   }
   value
 }
 
-set_number_value <- function(control, default, override) {
+set_number_value <- function(control, default, fallback) {
   is_valid <- function(x) length(x) == 1
   if (is_valid(default)) {
     value <- default
-  } else if (is_valid(override$value)) {
-    value <- override$value
+  } else if (is_valid(fallback)) {
+    value <- fallback
   } else {
     value <- NULL
   }
---
+++ tests/testthat/helper-options.R
@@ -0,0 +1,162 @@
+mwi_options <- list(
+  area_level = list(
+    list(
+      id = "1",
+      label = "1"
+    ),
+    list(
+      id = "2",
+      label = "2"
+    ),
+    list(
+      id = "3",
+      label = "3"
+    ),
+    list(
+      id = "4",
+      label = "4"
+    ),
+    list(
+      id = "5",
+      label = "5"
+    )
+  ),
+  calendar_quarter_t1 = list(
+    list(
+      id = "CY2016Q1",
+      label = "CY2016Q1"
+    )
+  ),
+  survey_prevalence = list(
+    list(
+      id = "MWI2016PHIA",
+      label = "MWI2016PHIA"
+    ),
+    list(
+      id = "test",
+      label = "test"
+    )
+  ),
+  survey_art_coverage = list(
+    list(
+      id = "MWI2016PHIA",
+      label = "MWI2016PHIA"
+    ),
+    list(
+      id = "test",
+      label = "test"
+    )
+  ),
+  survey_recently_infected = list(
+    list(
+      id = "MWI2016PHIA",
+      label = "MWI2016PHIA"
+    ),
+    list(
+      id = "test",
+      label = "test"
+    )
+  )
+)
+
+cmr_options <- list(
+  area_level = list(
+    list(
+      id = "1",
+      label = "1"
+    ),
+    list(
+      id = "2",
+      label = "2"
+    ),
+    list(
+      id = "3",
+      label = "3"
+    )
+  ),
+  calendar_quarter_t1 = list(
+    list(
+      id = "CY2017Q3",
+      label = "CY2017Q3"
+    )
+  ),
+  survey_prevalence = list(
+    list(
+      id = "CMR2018DHS",
+      label = "CMR2018DHS"
+    ),
+    list(
+      id = "CMR2017PHIA",
+      label = "CMR2017PHIA"
+    )
+  ),
+  survey_art_coverage = list(
+    list(
+      id = "CMR2018DHS",
+      label = "CMR2018DHS"
+    ),
+    list(
+      id = "CMR2017PHIA",
+      label = "CMR2017PHIA"
+    )
+  ),
+  survey_recently_infected = list(
+    list(
+      id = "CMR2018DHS",
+      label = "CMR2018DHS"
+    ),
+    list(
+      id = "CMR2017PHIA",
+      label = "CMR2017PHIA"
+    )
+  )
+)
+
+ago_options <- list(
+  area_level = list(
+    list(
+      id = "1",
+      label = "1"
+    ),
+    list(
+      id = "2",
+      label = "2"
+    )
+  ),
+  calendar_quarter_t1 = list(
+    list(
+      id = "CY2015Q4",
+      label = "CY2015Q4"
+    )
+  ),
+  survey_prevalence = list(
+    list(
+      id = "AGO2015DHS",
+      label = "AGO2015DHS"
+    ),
+    list(
+      id = "test",
+      label = "test"
+    )
+  ),
+  survey_art_coverage = list(
+    list(
+      id = "AGO2015DHS",
+      label = "AGO2015DHS"
+    ),
+    list(
+      id = "test",
+      label = "test"
+    )
+  ),
+  survey_recently_infected = list(
+    list(
+      id = "AGO2015DHS",
+      label = "AGO2015DHS"
+    ),
+    list(
+      id = "test",
+      label = "test"
+    )
+  )
+)
