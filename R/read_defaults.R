d081c244e593c6471797f6d1d9983532c1ceb9ca
Robert Ashton <r.ashton@imperial.ac.uk>
2022-11-30 17:35:49 +0000

    Convert types when reading in default options

--- R/read_defaults.R
+++ R/read_defaults.R
@@ -1,5 +1,5 @@
 read_hardcoded_defaults <- function(iso3, controls) {
-  data <- utils::read.csv(system_file("default_options.csv"))
+  data <- read_default_options()
   if (!(iso3 %in% data$iso3)) {
     stop(t_("FAILED_DEFAULT_OPTIONS_ISO3", list(iso3 = iso3)))
   }
@@ -34,4 +34,38 @@ as_multiselect <- function(value) {
     return("")
   }
   strsplit(as.character(value), "; *")[[1]]
+}
+
+read_default_options <- function() {
+  ## We read the table and convert columns here because otherwise excel likes
+  ## change types after opening and saving which can lead to problems.
+  ## TODO: convert these types by inspecting the type of the control
+  default <- data.table::fread(
+    system_file("default_options.csv"),
+    colClasses = "character",
+    data.table = FALSE,
+    nThread = 1
+  )
+  integer_cols <- c(
+    "area_level", "anc_clients_year2", "anc_clients_year2_num_months",
+    "anc_prevalence_year1", "anc_prevalence_year2", "anc_art_coverage_year1",
+    "anc_art_coverage_year2", "max_iterations", "no_of_samples", "rng_seed",
+    "artattend_log_gamma_offset", "deff_prev", "deff_artcov", "deff_recent",
+    "deff_vls")
+  truthy_cols <- c(
+    "use_survey_aggregate", "include_art_t1", "include_art_t2", "artattend",
+    "artattend_t2", "output_aware_plhiv", "permissive", "use_kish_prev",
+    "use_kish_artcov", "use_kish_recent", "use_kish_vls", "rho_paed_x_term",
+    "rho_paed_15to49f_ratio", "alpha_xst_term", "adjust_area_growth",
+    "anchor_home_district")
+
+  for (column in integer_cols) {
+    default[[column]] <- as.integer(default[[column]])
+  }
+  for (column in truthy_cols) {
+    # The actual values we want to match here are "true", "false" as strings
+    # so convert truthy cols into lower case
+    default[[column]] <- tolower(default[[column]])
+  }
+  default
 }
